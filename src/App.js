744b89e310739aec4210aa3e054239e728dbf5ff
Paul Dennig <pauldennig@berkeley.edu>
2023-04-03 01:12:46 -0700

    completed navbar template

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
-import React from 'react';
-import Navbar from './components/Navbar'
+import React, { useState } from 'react';
+
+//import Navbar from './components/Navbar'
 //import logo from './assets/logo.png';
 
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
-      <h1>CLTA</h1>
+      <Navbar>
+        <NavItem icon='hello'>
+          <DropdownMenu/>
+        </NavItem>
+      </Navbar>
+      <h1>CantoLTA</h1>
     </div>
   );
+}
+
+function Navbar(props) {
+  return (
+    <nav className='navbar'>
+      <ul className='navbar-nav'> { props.children } </ul>
+    </nav>
+  )
+}
+
+function NavItem(props) {
+  const [open, setOpen] = useState(false);
+  return (
+    <li className='nav-item'>
+      <a href="#" className='icon-button' onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+      {open && props.children}
+    </li>
+  )
+}
+
+function DropdownMenu() {
+
+  function DropdownItem(props) {
+    return (
+      <a href='#' className='menu-item'>
+        <span className='icon-button'>{props.leftIcon}</span>
+        {props.children}
+        <span className='icon-right'>{props.rightIcon}</span>
+      </a>
+    )
+  }
+
+  return (
+    <div className='dropdown'>
+      <DropdownItem>hi there</DropdownItem>
+    </div>
+  )
 }
 
 export default App;
