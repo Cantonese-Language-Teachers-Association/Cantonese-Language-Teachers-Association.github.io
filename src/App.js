9a44a9e9617e90178bad99b7b832d5ac63eaf416
Paul Dennig <pauldennig@berkeley.edu>
2023-04-02 17:56:53 -0700

    added new css and removed bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import logo from './assets/logo.png';
-//import './App.css';
+import React from 'react';
+//import logo from './assets/logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ...from the Cantonese Alliance.
-        </p>
-      </header>
+    <div>
+      <h1>CLTA</h1>
     </div>
   );
 }
 
 export default App;
+
+/*
+<div className="App">
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      ...from the Cantonese Alliance.
+    </p>
+  </header>
+</div>
+*/
